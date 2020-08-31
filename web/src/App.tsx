253aeaca45f36bdecf61bd697e166dfe2c753935
Gustavo Oliveira <gustavo.gcdo@gmail.com>
2020-08-31 18:50:07 -0400

    added initial web project structure

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app-container">
+      to com fome app
+    </div>
+  );
+}
+
+export default App;
