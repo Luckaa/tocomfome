3ed3d79e7938a1bd97cbcf89298bd24419826b76
Lucas <lucasilva137@gmail.com>
2020-09-29 22:31:58 -0400

    adicionado a configuração de rota no APP

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,6 +2,14 @@ import express, { Application } from 'express';
 import cors from 'cors';
 import { configureEnv } from './config';
 import { LoginRoutes } from './presentation/routes/login.routes';
+import { InfoRoutes } from './presentation/routes/info.routes';
+import { RegisterClientRoutes } from './presentation/routes/register-client.routes';
+import { RegisterRestaurantRoutes } from './presentation/routes/register-restaurant.routes';
+import { BuscarRestaurantRoutes } from './presentation/routes/buscar-restaurant.routes';
+
+
+
+
 
 export class App {
   private app: Application;
@@ -25,7 +33,26 @@ export class App {
     const loginRoute = new LoginRoutes();
     const loginRoutes = loginRoute.getRoutes();
 
+    const defaultRoute = new InfoRoutes();
+    const defaultRoutes = defaultRoute.getRoutes();
+
+
+    const buscarRestaurantRoute = new BuscarRestaurantRoutes();
+    const buscarRestaurantRoutes = buscarRestaurantRoute.getRoutes();
+
+    const registerClientRoute = new RegisterClientRoutes();
+    const registerClientRoutes = registerClientRoute.getRoutes();
+
+    const registerRestaurantRoute = new RegisterRestaurantRoutes();
+    const registerRestaurantRoutes = registerRestaurantRoute.getRoutes();
+
+    
     this.app.use(loginRoutes);
+    this.app.use(defaultRoutes);
+    this.app.use(registerClientRoutes);
+    this.app.use(registerRestaurantRoutes);
+    this.app.use(buscarRestaurantRoutes)
+
   }
 
   public start() {
