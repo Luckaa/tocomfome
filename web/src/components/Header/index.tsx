81d963170806ac2db5074f71463287a68c6e89a9
Gustavo Oliveira <gustavo.gcdo@gmail.com>
2020-12-23 16:21:38 -0400

    added initial structure for header

---
+++ web/src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return <div>I'am header</div>;
+}
+
+export default Header;
--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Header from '../../components/Header/index';
+import './header.scss';
 
 const Home: React.FC = () => {
-  return <div>Page home</div>;
+  return (
+    <div className="home-container">
+      <Header />
+      Page home
+    </div>
+  );
 };
 
 export default Home;
