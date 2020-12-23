3cf97c8645166d96feb112476383f725577578b1
Gustavo Oliveira <gustavo.gcdo@gmail.com>
2020-12-23 16:16:11 -0400

    added page for create screen home

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Redirect, Route } from "react-router-dom";
+import Home from './pages/Home/index';
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 const Routes = () => {
   return (
     <BrowserRouter>
+      <Redirect path="/" to="/home" />
       <Route component={Login} path="/login" />
       <Route component={Register} path="/register" />
+      <Route component={Home} path="/home" />
     </BrowserRouter>
   );
 };
