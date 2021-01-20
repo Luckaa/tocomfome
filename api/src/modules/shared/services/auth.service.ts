163a51d5fd6866cc78efd214ab06ddb69a6a273d
Gustavo Oliveira <gustavo.gcdo@gmail.com>
2021-01-19 21:06:50 -0400

    change verifyAsync for verify

--- api/src/modules/shared/services/auth.service.ts
+++ api/src/modules/shared/services/auth.service.ts
@@ -1,11 +1,8 @@
 import { sign, verify } from 'jsonwebtoken';
-import { promisify } from 'util';
 import config from '../../../config';
 import { User } from '../../users/models/user';
 import { Payload } from '../models/payload';
 import { IAuthService } from './auth-service.interface';
-
-const verifyAsync = promisify(verify);
 
 export class AuthService implements IAuthService {
 
@@ -22,7 +19,7 @@ export class AuthService implements IAuthService {
 
     async verifyToken(token: string): Promise<Payload> {
         const tokenReplace = this.replaceToken(token);
-        const data: any = await verifyAsync(tokenReplace, config.SALT_KEY);
+        const data: any = await verify(tokenReplace, config.SALT_KEY);
         return data;
     }
 
